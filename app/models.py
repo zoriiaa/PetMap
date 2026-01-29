68ee121bc04ff67d40bff758b758549b1155b52b
Petrivska Zoreslava <petrivska.zzoreslava@gmail.com>
2026-01-29 17:18:33 +0200

    Uploaded models.py

--- app/models.py
+++ app/models.py
@@ -1,17 +1,25 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 from datetime import datetime
 from sqlalchemy import  func
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    tablename = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False, index=True)
-    email = db.Column(db.String, unique=True, nullable=False, index=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     psw = db.Column(db.String(128), nullable=False)
 
     pets = db.relationship('Pet', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.psw = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.psw, password)
 
 class Pet(db.Model):
     __tablename__ = 'pets'
